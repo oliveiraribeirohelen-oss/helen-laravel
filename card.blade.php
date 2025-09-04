e0a6b0d53c7522b01dc553b541b220a02f87e47f
oliveiraribeirohelen@gmail.com <oliveiraribeirohelen@gmail.com>
2025-09-04 16:51:15 -0300

    Adiciona páginas chef, card e profile

---
+++ card.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Desenvolvedor em treinamento">
+    <meta name="Helen de Oliveira Ribeiro">
+    <title>Helen de Oliveira Ribeiro</title>
+</head>
+<body>
+<main>
+    <h1>Helen de Oliveira Ribeiro</h1>
+    <p>Eu sou a Helen, tenho 16 anos e gosto de muito de ler.</p>
+    <p> Meu e-mail:oliveiraribeirohelen@gmail.com</p>
+</main>
+<footer>
+    <p>2025 Onde há mar, há de haver esperança.</p>
+</footer>
+</body>
+</html>
