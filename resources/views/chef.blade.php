7ad39d390701113a4df16f2fb085e4ad462c6955
sofia <sofia.fsenai@gmail.com>
2025-09-04 10:14:27 -0300

    Adicionar controlador PrincipalController e views

---
+++ resources/views/chef.blade.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receita de Brownie</title>
+</head>
+<body>
+    <h1>Receita do Brownie Perfeito</h1>
+
+    <h2>Ingredientes</h2>
+    <ul>
+        <li>200g de chocolate meio amargo</li>
+        <li>150g de manteiga</li>
+        <li>1 xícara e meia de açúcar</li>
+        <li>3 ovos</li>
+        <li>1 xícara de farinha de trigo</li>
+        <li>1 xícara e meia de Nescau</li>
+        <li>1 pitada de sal</li>
+        <li>1/2 xícara de nozes (opcional)</li>
+    </ul>
+
+    <h2>Modo de Preparo</h2>
+    <ol>
+        <li>Preaqueça o forno a 180°C.</li>
+        <li>Derreta o chocolate e a manteiga em banho-maria ou no micro-ondas.</li>
+        <li>Em uma tigela, bata os ovos com o açúcar até formar uma mistura fofa e clara.</li>
+        <li>Adicione o chocolate derretido à mistura de ovos e mexa bem.</li>
+        <li>Acrescente a farinha, o Nescau e a pitada de sal. Misture até ficar homogêneo.</li>
+        <li>Se desejar, adicione as nozes picadas.</li>
+        <li>Despeje a massa em uma forma untada e enfarinhada.</li>
+        <li>Leve ao forno por cerca de 25 a 30 minutos, ou até que ao espetar um palito, ele saia com algumas migalhas úmidas.</li>
+        <li>Deixe esfriar, corte em pedaços e sirva!</li>
+    </ol>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><strong>Helen de Oliveira Ribeiro</strong></h1>
+    <p><strong>Tenho 16 anos, gosto de ler, minha matéria favorita é Inglês, amo cozinhar e meu gênero musical favorito é samba/pagode. Quero cursar Nutrição na faculdade</strong></p>
+    <strong>Cozinho muito bem, me dou bem com crianças e sou engraçada</strong>
+    <h1>Meus hobbies:</h1>
+    <ul>
+        <li>ler</li>
+        <li>cozinhar</li>
+        <li>fazer academia</li>
+        <li>pintar</li>
+        <li>fazer esculturas com argila</li>
+        <li>assistir series e filmes</li>
+        <li>fotografia</li>
+        <li>escutar música</li>
+    </ul>
+
+</body>
+</html>
